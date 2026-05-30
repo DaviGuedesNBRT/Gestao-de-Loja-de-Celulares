a0744a3bc0447f711aa165e467c3022db6fd8f14
DaviGuedesNBRT <daviguedesnoberto@outlook.com>
2026-05-30 15:21:26 -0300

    prints

---
+++ main.py
@@ -0,0 +1,7 @@
+print("""
+======================
+1 - Gerir Clientes
+2 - Gerir Estoque
+3 - Gerir Vendas
+=======================
+""")
---
+++ usuarios.py
@@ -0,0 +1,10 @@
+print("""
+=======================
+1 - Cadastrar Cliente
+2 - Editar Cliente
+3 - visualizar Cliente
+4 - Visualizar Clientes
+5 - Excluir Cliente
+6 - Atualizar Cliente
+=======================
+""")
